186d71da23d350ab4987f3764d37cf915697d00e
StarMac <starmac2008@gmail.com>
2022-04-20 17:01:00 +0300

    Added Lab 2 Computer Graphics

---
+++ Computer graphics/Lab 2/Lab2ComputerGraphics/Lab2ComputerGraphics/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace Lab2ComputerGraphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.label_X = new System.Windows.Forms.Label();
+            this.label_Y = new System.Windows.Forms.Label();
+            this.label_Radius = new System.Windows.Forms.Label();
+            this.textBox_X = new System.Windows.Forms.TextBox();
+            this.textBox_Y = new System.Windows.Forms.TextBox();
+            this.textBox_Radius = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_Bresenhem = new System.Windows.Forms.TextBox();
+            this.textBox_My = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(19, 101);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1013, 576);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(860, 23);
+            this.Start.Margin = new System.Windows.Forms.Padding(4);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(169, 50);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Draw";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // label_X
+            // 
+            this.label_X.AutoSize = true;
+            this.label_X.Location = new System.Drawing.Point(16, 19);
+            this.label_X.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_X.Name = "label_X";
+            this.label_X.Size = new System.Drawing.Size(17, 17);
+            this.label_X.TabIndex = 3;
+            this.label_X.Text = "X";
+            // 
+            // label_Y
+            // 
+            this.label_Y.AutoSize = true;
+            this.label_Y.Location = new System.Drawing.Point(16, 54);
+            this.label_Y.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_Y.Name = "label_Y";
+            this.label_Y.Size = new System.Drawing.Size(17, 17);
+            this.label_Y.TabIndex = 4;
+            this.label_Y.Text = "Y";
+            // 
+            // label_Radius
+            // 
+            this.label_Radius.AutoSize = true;
+            this.label_Radius.Location = new System.Drawing.Point(198, 18);
+            this.label_Radius.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_Radius.Name = "label_Radius";
+            this.label_Radius.Size = new System.Drawing.Size(52, 17);
+            this.label_Radius.TabIndex = 5;
+            this.label_Radius.Text = "Radius";
+            // 
+            // textBox_X
+            // 
+            this.textBox_X.Location = new System.Drawing.Point(43, 15);
+            this.textBox_X.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_X.Name = "textBox_X";
+            this.textBox_X.Size = new System.Drawing.Size(132, 22);
+            this.textBox_X.TabIndex = 6;
+            // 
+            // textBox_Y
+            // 
+            this.textBox_Y.Location = new System.Drawing.Point(43, 51);
+            this.textBox_Y.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Y.Name = "textBox_Y";
+            this.textBox_Y.Size = new System.Drawing.Size(132, 22);
+            this.textBox_Y.TabIndex = 7;
+            // 
+            // textBox_Radius
+            // 
+            this.textBox_Radius.Location = new System.Drawing.Point(260, 14);
+            this.textBox_Radius.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Radius.Name = "textBox_Radius";
+            this.textBox_Radius.Size = new System.Drawing.Size(132, 22);
+            this.textBox_Radius.TabIndex = 8;
+            this.textBox_Radius.TextChanged += new System.EventHandler(this.textBox_Radius_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(485, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "The Bresenham Algorithm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(485, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "My algorithm";
+            // 
+            // textBox_Bresenhem
+            // 
+            this.textBox_Bresenhem.Enabled = false;
+            this.textBox_Bresenhem.Location = new System.Drawing.Point(665, 15);
+            this.textBox_Bresenhem.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Bresenhem.Name = "textBox_Bresenhem";
+            this.textBox_Bresenhem.Size = new System.Drawing.Size(68, 22);
+            this.textBox_Bresenhem.TabIndex = 11;
+            // 
+            // textBox_My
+            // 
+            this.textBox_My.Enabled = false;
+            this.textBox_My.Location = new System.Drawing.Point(665, 60);
+            this.textBox_My.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_My.Name = "textBox_My";
+            this.textBox_My.Size = new System.Drawing.Size(68, 22);
+            this.textBox_My.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(742, 19);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "ms";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(742, 63);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "ms";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 690);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_My);
+            this.Controls.Add(this.textBox_Bresenhem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Radius);
+            this.Controls.Add(this.textBox_Y);
+            this.Controls.Add(this.textBox_X);
+            this.Controls.Add(this.label_Radius);
+            this.Controls.Add(this.label_Y);
+            this.Controls.Add(this.label_X);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Raster graphics";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label label_X;
+        private System.Windows.Forms.Label label_Y;
+        private System.Windows.Forms.Label label_Radius;
+        private System.Windows.Forms.TextBox textBox_X;
+        private System.Windows.Forms.TextBox textBox_Y;
+        private System.Windows.Forms.TextBox textBox_Radius;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_Bresenhem;
+        private System.Windows.Forms.TextBox textBox_My;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Computer graphics/Lab 2/Lab2ComputerGraphics/Lab2ComputerGraphics/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2ComputerGraphics
+{
+    public partial class Form1 : Form
+    {
+        Brush brush;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void MyAlgorithm()
+        {
+            brush = Brushes.Red;
+
+            var g = Graphics.FromImage(pictureBox1.Image);
+
+            var R = Convert.ToDouble(textBox_Radius.Text);
+            var X = Convert.ToInt32(textBox_X.Text) * 2;
+            var Y = Convert.ToInt32(textBox_Y.Text);
+
+            for (int i = 0; i < 360; i++)
+            {
+                var x = X + Math.Round(R * Math.Sin(i * Math.PI / 180));
+                var y = Y - Math.Round(R * Math.Cos(i * Math.PI / 180));
+                g.FillRectangle(brush, (int)x, (int)y, 1, 1);
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void BresenhemAlgorithm()
+        {
+            brush = Brushes.Blue;
+
+            var g = Graphics.FromImage(pictureBox1.Image);
+
+            var R = Convert.ToDouble(textBox_Radius.Text);
+            var X = Convert.ToInt32(textBox_X.Text);
+            var Y = Convert.ToInt32(textBox_Y.Text);
+
+            int x = 0, y = (int)R, gap, delta = (int)(2 - 2 * R);
+
+            while (y >= 0)
+            {
+                g.FillRectangle(brush, (X + x), (Y + y), 1, 1);
+                g.FillRectangle(brush, (X + x), (Y - y), 1, 1);
+                g.FillRectangle(brush, (X - x), (Y - y), 1, 1);
+                g.FillRectangle(brush, (X - x), (Y + y), 1, 1);
+
+                gap = 2 * (delta + y) - 1;
+                if (delta < 0 && gap <= 0)
+                {
+                    x++;
+                    delta += 2 * x + 1;
+                    continue;
+                }
+                if (delta > 0 && gap > 0)
+                {
+                    y--;
+                    delta -= 2 * y + 1;
+                    continue;
+                }
+                x++;
+                delta += 2 * (x - y);
+                y--;
+            }
+            pictureBox1.Refresh();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            var stopwatchMyAlgorithm = new Stopwatch();
+            var stopwatchBresenhemAlgorithm = new Stopwatch();
+
+            try
+            {
+                stopwatchMyAlgorithm.Start();
+                MyAlgorithm();
+                stopwatchMyAlgorithm.Stop();
+
+                stopwatchBresenhemAlgorithm.Start();
+                BresenhemAlgorithm();
+                stopwatchBresenhemAlgorithm.Stop();
+            }
+
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+
+                textBox_X.Text = null;
+                textBox_Y.Text = null;
+                textBox_Radius.Text = null;
+                textBox_Bresenhem.Text = null;
+                textBox_My.Text = null;
+            }
+
+            textBox_Bresenhem.Text = stopwatchBresenhemAlgorithm.ElapsedMilliseconds.ToString();
+            textBox_My.Text = stopwatchMyAlgorithm.ElapsedMilliseconds.ToString();
+        }
+
+        private void textBox_Radius_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
